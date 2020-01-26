c6445d8f9bf996623703ab96050438b4a896b010
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-01-26 23:01:35 +0900

    impl: スタミナ回復ダイアログ実装

---
+++ Assets/Scripts/UI/UIRecoveryStaminaDialog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MokomoGames;
+using TMPro;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MokomoGames
+{
+    public class UIRecoveryStaminaDialog : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI hasYukichiNumText;
+        [SerializeField] private UIStamina stamina;
+        [SerializeField] private TextMeshProUGUI consumeYukichi;
+        [SerializeField] private Button lawButton;
+        [SerializeField] private Button yesButton;
+        [SerializeField] private Button noButton;
+        [SerializeField] private Button closeButton;
+
+        public event Action OnTappedLawButton;
+        public event Action OnTappedNoButton;
+        public event Action OnTappedYesButton;
+        public event Action OnTappedCloseButton;
+
+        private void Awake()
+        {
+            lawButton.onClick.AddListener( () => OnTappedLawButton?.Invoke());
+            noButton.onClick.AddListener( () => OnTappedNoButton?.Invoke());
+            yesButton.onClick.AddListener( () => OnTappedYesButton?.Invoke());
+            closeButton.onClick.AddListener( () => OnTappedCloseButton?.Invoke());
+        }
+
+        public void Open()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Close()
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void Initialize(int currentStamina,int maxStamina,int yukichiNum,int consumeYukichiNum=1)
+        {
+            stamina.SetCurrentValue(currentStamina,maxStamina);
+            hasYukichiNumText.text = $"{yukichiNum}";
+            consumeYukichi.text = $"{consumeYukichiNum}枚";
+        }
+    }
+}
