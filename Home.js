841ff1ddd9d3dcf9dd8479ccea8f5c18fb7cf611
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-02-01 23:00:57 +0900

    fix: 時刻をUtcに

--- Home.js
+++ Home.js
@@ -1,6 +1,6 @@
 handlers.updateTimeWhenLeaving = function (args, context) {
     var data = new Date();
-    var formatedTime = data.getFullYear() + "_" + (data.getMonth() + 1) + "_" + data.getDate() + "_" + data.getHours() + "_" + data.getMinutes() + "_" + data.getSeconds();
+    var formatedTime = data.getUTCFullYear() + "_" + (data.getUTCMonth() + 1) + "_" + data.getUTCDate() + "_" + data.getUTCHours() + "_" + data.getUTCMinutes() + "_" + data.getUTCSeconds();
     server.UpdateUserData({
         PlayFabId: currentPlayerId,
         Data: { timeWhenLeaving: formatedTime }
--- Home.ts
+++ Home.ts
@@ -2,7 +2,7 @@ handlers.updateTimeWhenLeaving = (args,context) =>
 {
     const data = new Date();
     const formatedTime : string = 
-        `${data.getFullYear()}_${data.getMonth() + 1}_${data.getDate()}_${data.getHours()}_${data.getMinutes()}_${data.getSeconds()}`;
+        `${data.getUTCFullYear()}_${data.getUTCMonth() + 1}_${data.getUTCDate()}_${data.getUTCHours()}_${data.getUTCMinutes()}_${data.getUTCSeconds()}`;
     server.UpdateUserData({
         PlayFabId: currentPlayerId,
         Data:{timeWhenLeaving: formatedTime}
