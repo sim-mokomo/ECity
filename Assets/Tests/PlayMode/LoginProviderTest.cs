cdf75686c439ddf5d50aa401b1cc4287e9e70af4
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-02-01 18:54:03 +0900

    impl: ログイン処理のテスト作成

---
+++ Assets/Tests/PlayMode/LoginProviderTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using MokomoGames;
+
+namespace Tests
+{
+    public class LoginProviderTest
+    {
+        [UnityTest]
+        public IEnumerator LoginProviderTestWithEnumeratorPasses()
+        {
+            LoginProvider.LoginByEditor(customId: "LoginProviderTest",
+                result =>
+                {
+                    Assert.IsNotNull(result);
+                },
+                error => { });
+            yield return null;
+        }
+    }
+}
