571b1a0952602edcb9b3c0b1c2532974d5082da0
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-02-01 13:50:22 +0900

    impl: Editor/Mobileからログインできるように

---
+++ Assets/Scripts/LoginProvider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+namespace MokomoGames
+{
+    public class LoginProvider
+    {
+        public static void LoginByEditor(string customId,Action<LoginResult> onLoggedIn, Action<PlayFabError> onError = null)
+        {
+            PlayFabClientAPI.ForgetAllCredentials ();
+            var loginRequest = new LoginWithCustomIDRequest
+            {
+                CustomId = customId,
+                CreateAccount = true,
+            };
+            PlayFabClientAPI.LoginWithCustomID (loginRequest, onLoggedIn, onError);
+        }
+
+        public static void LoginByMobile (Action<LoginResult> onLoggedIn, Action<PlayFabError> onError = null)
+        {
+    #if UNITY_ANDROID
+            AndroidJavaClass clsUnity = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject objActivity = clsUnity.GetStatic<AndroidJavaObject> ("currentActivity");
+            AndroidJavaObject objResolver = objActivity.Call<AndroidJavaObject> ("getContentResolver");
+            AndroidJavaClass clsSecure = new AndroidJavaClass ("android.provider.Settings$Secure");
+            var android_id = clsSecure.CallStatic<string> ("getString", objResolver, "android_id");
+            var loginRequest = new LoginWithAndroidDeviceIDRequest
+            {
+                AndroidDeviceId = android_id,
+                CreateAccount = true,
+            };
+            PlayFabClientAPI.LoginWithAndroidDeviceID (loginRequest, OnLoggedIn, OnError);
+    #elif UNITY_IPHONE
+            var loginRequest = new LoginWithIOSDeviceIDRequest
+            {
+                DeviceId = UnityEngine.iOS.Device.vendorIdentifier,
+                CreateAccount = true,
+            };
+            PlayFabClientAPI.LoginWithIOSDeviceID (loginRequest, OnLoggedIn, OnError);
+    #endif
+        }
+    }
+}
---
+++ Assets/Scripts/MainGameController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MokomoGames;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+
+public class MainGameController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI loginIdText;
+
+    private void Start()
+    {
+        void OnLoggedIn(LoginResult result)
+        {
+            loginIdText.text = $"ID:{result.PlayFabId}";
+        }
+
+        void OnError(PlayFabError error)
+        {
+        }
+
+#if UNITY_EDITOR
+        LoginProvider.LoginByEditor(customId: "TestUser1", OnLoggedIn, OnError);
+#elif UNITY_ANDROID || UNITY_IPHONE
+        LoginProvider.LoginByMobile(OnLoggedIn, OnError);
+#endif
+    }
+}
