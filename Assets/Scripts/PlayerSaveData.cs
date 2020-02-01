918ab40d8095a9ddb100dc4f0f67870f38863f34
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-02-01 16:55:09 +0900

    impl: プレイヤーデータを保存取得できるように

---
+++ Assets/Scripts/PlayerSaveData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+namespace MokomoGames
+{
+    public class PlayerSaveData
+    {
+        [SerializeField] private float stamina;
+
+        public float Stamina => stamina;
+
+        public PlayerSaveData(float stamina)
+        {
+            this.stamina = stamina;
+        }
+
+        public Dictionary<string, string> ToSaveDic()
+        {
+            var saveDic = new Dictionary<string,string>();
+            saveDic.Add("stamina",Stamina.ToString(CultureInfo.InvariantCulture));
+            return saveDic;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerSaveDataRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+
+namespace MokomoGames
+{
+    public class PlayerSaveDataRepository
+    {
+        public static void UpdatePlayerSaveData(PlayerSaveData playerSaveData,Action onEndUpdate)
+        {
+            var saveDic = playerSaveData.ToSaveDic();
+            PlayFabClientAPI.ExecuteCloudScript(
+                new ExecuteCloudScriptRequest()
+                {
+                    FunctionName = "updatePlayerSaveData",
+                    FunctionParameter = saveDic,
+                    GeneratePlayStreamEvent = true,
+                },
+                result => { onEndUpdate?.Invoke(); },
+                error => { });
+        }
+        
+        public static void GetPlayerSaveData(PlayerSaveData playerSaveData)
+        {
+            PlayFabClientAPI.ExecuteCloudScript
+            (
+                new ExecuteCloudScriptRequest()
+                {
+                    FunctionName = "getPlayerSaveData",
+                    GeneratePlayStreamEvent = true,
+                },
+                scriptResult =>
+                {
+                    JsonUtilityExtensions.FunctionResult2Instance<PlayerSaveData>(scriptResult.FunctionResult,playerSaveData);
+                },
+                error => { }
+            );
+        }
+    }
+}
