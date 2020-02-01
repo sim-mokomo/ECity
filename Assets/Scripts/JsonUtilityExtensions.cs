9bcf3778955f645b24c15686edddab00d16b9fa7
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-02-01 16:52:57 +0900

    impl: FunctionResultからインスタンスに変換できるように

---
+++ Assets/Scripts/JsonUtilityExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class JsonUtilityExtensions
+{
+    public static T FunctionResult2Instance<T>(object functionResult,T writeObj)
+    {
+        var json = functionResult.ToString();
+        Debug.Log(json);
+        JsonUtility.FromJsonOverwrite(json,writeObj);
+        return writeObj;
+    }
+    
+    public static T FunctionResult2Instance<T> (object functionResult)
+    {
+        var json = functionResult.ToString ();
+        Debug.Log (json);
+        return JsonUtility.FromJson<T> (json);
+    }
+}
