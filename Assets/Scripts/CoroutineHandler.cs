f93b4e86c02edf60e87b7c55619453ea23081668
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-02-01 22:21:37 +0900

    impl: Monobehaviour継承なしでもCoroutine作成できるように

---
+++ Assets/Scripts/CoroutineHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineHandler : SingletonMonobehaivor<CoroutineHandler>
+{
+    public void StopCoroutine (Coroutine handler)
+    {
+        if (handler == null)
+            return;
+        base.StopCoroutine (handler);
+        handler = null;
+    }
+}
---
+++ Assets/Scripts/SingletonMonobehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SingletonMonobehaivor<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                Type type = typeof(T);
+                _instance = FindObjectOfType(type) as T;
+                if(_instance == null)
+                {
+                    Debug.LogError($"{type.Name}がシーンに見つかりませんでした。");
+                }
+            }
+            return _instance;
+        }
+    }
+
+}
