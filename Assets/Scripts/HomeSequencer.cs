dddaca6dc78488500d33eb9296c899829bbe720e
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-02-08 18:04:44 +0900

    impl: home sequencer にスタミナ回復処理移動

---
+++ Assets/Scripts/HomeSequencer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MokomoGames
+{
+    public class HomeSequencer : MonoBehaviour, ISequencer
+    {
+        public MasterSequencer.SequencerType Type => MasterSequencer.SequencerType.Home;
+        public event Action<MasterSequencer.SequencerType, bool, Func<bool>> OnLeave;
+        [SerializeField] private HomeScreenController homeScreenController;
+
+        public void Begin()
+        {
+            Display(true);
+            homeScreenController.Begin();
+        }
+
+        public void Tick()
+        {
+            homeScreenController.Tick();
+        }
+
+        public void End()
+        {
+            homeScreenController.End();
+            Display(false);
+        }
+
+        public void Display(bool show)
+        {
+            gameObject.SetActive(show);
+        }
+    }
+}
