24e44aea86054ac7ab6923934379a9224940db9c
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-02-08 17:49:28 +0900

    impl: load画面用シーケンサー作成

---
+++ Assets/Scripts/ISequencer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MokomoGames;
+using UnityEngine;
+
+namespace MokomoGames
+{
+    public interface ISequencer
+    {
+        MasterSequencer.SequencerType Type { get; }
+        event Action<MasterSequencer.SequencerType> OnLeave;
+        void Begin();
+        void Tick();
+        void End();
+        void Display(bool show);
+    }
+}
---
+++ Assets/Scripts/LoadingSequencer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MokomoGames
+{
+    public class LoadingSequencer : MonoBehaviour, ISequencer
+    {
+        public MasterSequencer.SequencerType Type => MasterSequencer.SequencerType.Loading;
+        public event Action<MasterSequencer.SequencerType> OnLeave;
+        private event Func<bool> leaveCondition;
+        public MasterSequencer.SequencerType DistSequencer { get; private set; }
+
+        public void Begin()
+        {
+            Display(true);
+            StartCoroutine(Wait());
+        }
+
+        private IEnumerator Wait()
+        {
+            while (!leaveCondition())
+            {
+                yield return null;
+            }
+
+            OnLeave?.Invoke(DistSequencer);
+        }
+
+        public void Tick()
+        {
+        }
+
+        public void End()
+        {
+            Display(false);
+        }
+
+        public void Display(bool show)
+        {
+            gameObject.SetActive(show);
+        }
+
+        public void SetConfiguraiton(Func<bool> leaveCondition, MasterSequencer.SequencerType distType)
+        {
+            this.leaveCondition = leaveCondition;
+            this.DistSequencer = distType;
+        }
+    }
+}
