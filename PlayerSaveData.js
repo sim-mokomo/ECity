5f3fc8c798fa3352a910f1e2f46c06c24fe13d8d
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-02-13 23:49:28 +0900

    delete: 不要なファイル削除

--- PlayerSaveData.js
+++
@@ -1,6 +0,0 @@
-var PlayerSaveData = /** @class */ (function () {
-    function PlayerSaveData(stamina) {
-        this.stamina = stamina;
-    }
-    return PlayerSaveData;
-}());
--- PlayerSaveData.ts
+++
@@ -1,8 +0,0 @@
-class PlayerSaveData
-{
-    public stamina:number;
-
-    constructor(stamina:number) {
-        this.stamina = stamina;
-    }
-}
