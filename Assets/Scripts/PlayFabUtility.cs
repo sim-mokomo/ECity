c2fe18c41b73f9cca3f8c5441e61d67e443a3b18
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-02-24 00:13:26 +0900

    impl: AzureFunctionと通信

---
+++ Assets/Scripts/PlayFabUtility.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using PlayFab;
+
+namespace MokomoGames
+{
+    public class PlayFabUtility
+    {
+        public static PlayFab.CloudScriptModels.EntityKey CreateEntityKey()
+        {
+            return new PlayFab.CloudScriptModels.EntityKey()
+            {
+                Id = MainGameController.UserDataContainer.PlayFabAuthenticationContext.EntityId,
+                Type = MainGameController.UserDataContainer.PlayFabAuthenticationContext.EntityType
+            };
+        }
+        
+        public static void GenerateErrorReport(PlayFabError error)
+        {
+            Debug.Log($"Opps Something went wrong: {error.GenerateErrorReport()}");
+            Debug.Log(error.HttpCode);
+            Debug.Log(error.HttpStatus);
+            if(error.ErrorDetails == null)
+                return;
+            foreach (var detail in error.ErrorDetails)
+            {
+                Debug.Log($"{detail.Key}/{detail.Value}");
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerSaveDataRepository.cs
+++ Assets/Scripts/PlayerSaveDataRepository.cs
@@ -1,58 +1,36 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Google.Protobuf;
 using PlayFab;
 using PlayFab.ClientModels;
+using MokomoGames.Protobuf;
+using PlayFab.CloudScriptModels;
+using PlayFab.Json;
 using UnityEngine;
-
 
 namespace MokomoGames
 {
     public class PlayerSaveDataRepository
     {
-        public static void UpdatePlayerSaveData(PlayerSaveData playerSaveData,Action onEndUpdate)
+        public static void GetPlayerSaveData(Action<GetPlayerSaveDataResponse> onEndGetSaveData)
         {
-            var saveDic = playerSaveData.ToSaveDic();
-            PlayFabClientAPI.ExecuteCloudScript(
-                new ExecuteCloudScriptRequest()
+            PlayFabCloudScriptAPI.ExecuteFunction(
+                new ExecuteFunctionRequest()
                 {
-                    FunctionName = "updatePlayerSaveData",
-                    FunctionParameter = saveDic,
-                    GeneratePlayStreamEvent = true,
-                },
-                result => { onEndUpdate?.Invoke(); },
-                error => { });
-        }
-        
-        public static void GetPlayerSaveData(PlayerSaveData playerSaveData,Action<PlayerSaveData> onEndGetSaveData)
-        {
-            PlayFabClientAPI.ExecuteCloudScript
-            (
-                new ExecuteCloudScriptRequest()
-                {
+                    Entity = PlayFabUtility.CreateEntityKey(),
                     FunctionName = "getPlayerSaveData",
-                    GeneratePlayStreamEvent = true,
+                    FunctionParameter = null,
+                    GeneratePlayStreamEvent = true
                 },
-                scriptResult =>
+                result =>
                 {
-                    JsonUtilityExtensions.FunctionResult2Instance<PlayerSaveData>(scriptResult.FunctionResult,playerSaveData);
-                    onEndGetSaveData?.Invoke(playerSaveData);
+                    Debug.Log(result.FunctionResult.GetType());
+                    var response = GetPlayerSaveDataResponse.Parser.ParseJson(result.FunctionResult.ToString());
+                    onEndGetSaveData?.Invoke(response);
                 },
-                error => { }
-            );
-        }
-
-        public static void UpdateStamina(int diff,Action onEndUpdate)
-        {
-            PlayFabClientAPI.ExecuteCloudScript(
-                new ExecuteCloudScriptRequest()
-                {
-                    FunctionName = "updateStamina",
-                    FunctionParameter = new Dictionary<string,int>(){{"diff",diff}},
-                    GeneratePlayStreamEvent = true,
-                },
-                result => { onEndUpdate?.Invoke(); },
-                error => { });
+                PlayFabUtility.GenerateErrorReport);
         }
     }
 }
