eac2cc596f6c102e1be0940278ffa546d6d43260
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-02-23 20:15:12 +0900

    fix: AzureFunction移行によりtsをすべて削除

--- Home.js
+++
@@ -1,8 +0,0 @@
-handlers.updateTimeWhenLeaving = function (args, context) {
-    var data = new Date();
-    var formatedTime = data.getUTCFullYear() + "_" + (data.getUTCMonth() + 1) + "_" + data.getUTCDate() + "_" + data.getUTCHours() + "_" + data.getUTCMinutes() + "_" + data.getUTCSeconds();
-    server.UpdateUserData({
-        PlayFabId: currentPlayerId,
-        Data: { timeWhenLeaving: formatedTime }
-    });
-};
