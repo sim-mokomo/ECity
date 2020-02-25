b4af0de6b915e333d3ab39a96abf07d5021f2406
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-02-25 22:13:00 +0900

    impl: 早すぎるファイル分けなので削除

--- Assets/Scripts/HomeSequencer.cs
+++ Assets/Scripts/HomeSequencer.cs
@@ -1,36 +1,68 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using MokomoGames.Protobuf;
 using UnityEngine;
+using Zenject;
 
 namespace MokomoGames
 {
     public class HomeSequencer : MonoBehaviour, ISequencer
     {
         public MasterSequencer.SequencerType Type => MasterSequencer.SequencerType.Home;
+        
+        [SerializeField] private UIHeader headerUi;
+        [Inject] private IPlayerSaveDataRepository _playerSaveDataRepository;
+        private StaminaRecoveryTimeController staminaRecoveryTimeController;
+        private PlayerSaveData saveData;
         public event Action<MasterSequencer.SequencerType, bool, Func<bool>> OnLeave;
-        [SerializeField] private HomeScreenController homeScreenController;
-
+        
         public void Begin()
         {
             Display(true);
-            homeScreenController.Begin();
+
+            staminaRecoveryTimeController = new StaminaRecoveryTimeController(10,_playerSaveDataRepository);
+            staminaRecoveryTimeController.OnRecoveriedStamina += () =>
+            {
+                saveData.Stamina += 1;
+                Refresh(saveData);
+            };
+            staminaRecoveryTimeController.OnClock += () =>
+            {
+                headerUi.SetStaminaTime(
+                    staminaRecoveryTimeController.Minutes,
+                    staminaRecoveryTimeController.Seconds);
+            };
+            staminaRecoveryTimeController.Begin();
+        
+            _playerSaveDataRepository.GetPlayerSaveData(responseSaveData =>
+            {
+                saveData = responseSaveData;
+                Refresh(saveData);
+            });
         }
 
         public void Tick()
         {
-            homeScreenController.Tick();
+            
         }
 
         public void End()
         {
-            homeScreenController.End();
             Display(false);
         }
 
         public void Display(bool show)
         {
             gameObject.SetActive(show);
+        }
+        
+        private void Refresh(PlayerSaveData save)
+        {
+            headerUi.SetStamina(save.Stamina,999);
+            headerUi.SetCoinNum(save.Coin);
+            headerUi.SetMizuNum(save.Mizu);
+            headerUi.SetYukichiNum(save.Yukichi);
         }
     }
 }
