c8b736a37629b85fd35bc87f4c6a06b7e514401a
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-02-25 22:23:02 +0900

    impl: ランク確認画面

---
+++ Assets/Scripts/UI/UIRankConfirm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace MokomoGames.UI
+{
+    public class UIRankConfirm : MonoBehaviour
+    {
+        [SerializeField] private UIGaugeWithUpperLabel expGauge;
+        [SerializeField] private UIGaugeWithUpperLabel staminaGauge;
+        [SerializeField] private TextMeshProUGUI currentRankText;
+        
+        public void SetCurrentRank(uint rank)
+        {
+            currentRankText.text = rank.ToString();
+        }
+        
+        public void SetExpGauge(uint remainingExp,uint maxExp)
+        {
+            expGauge.SetRemainingValue($"あと{remainingExp}");
+            expGauge.SetRemainingSliderValue(remainingExp,maxExp);
+        }
+
+        public void SetStaminaGauge(uint minutes,uint seconds,uint maxRecoverySeconds)
+        {
+            staminaGauge.SetRemainingValue($"あと{minutes}:{seconds}");
+            staminaGauge.SetRemainingSliderValue(minutes * 60 + seconds,maxRecoverySeconds);
+        }
+    }
+}
