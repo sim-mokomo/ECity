4f4647d1f903b4ff423f25787d241a27ecf993e4
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-02-25 22:22:32 +0900

    impl: 上付きゲージUIアイテム作成

---
+++ Assets/Scripts/UI/UIGaugeWithUpperLabel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MokomoGames
+{
+    public class UIGaugeWithUpperLabel : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI remainingValueText;
+        [SerializeField] private Slider remainingSlider;
+
+        public void SetRemainingValue(string valueStr)
+        {
+            remainingValueText.text = valueStr;
+        }
+
+        public void SetRemainingSliderValue(float current, float max)
+        {
+            remainingSlider.maxValue = max;
+            remainingSlider.value = current;
+        }
+    }
+}
