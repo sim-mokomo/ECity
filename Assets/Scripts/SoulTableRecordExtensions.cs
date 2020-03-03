6146b15c7ec0e49f343f6b947e95c05f0dc91c36
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-03-04 01:51:55 +0900

    impl: タブによって表示する魂を分けるように

---
+++ Assets/Scripts/SoulTableRecordExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MokomoGames.Protobuf;
+using UnityEngine;
+
+public static class SoulTableRecordExtensions
+{
+    public static bool IsMaterial(this SoulTableRecord self)
+    {
+        return self.SoulType == SoulType.Evolution ||
+               self.SoulType == SoulType.Sale ||
+               self.SoulType == SoulType.ReinforcedSynthesis;
+    }
+}
--- Assets/Scripts/UI/UISoulListPage.cs
+++ Assets/Scripts/UI/UISoulListPage.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using MokomoGames.Protobuf;
 using UnityEngine;
 using UnityEngine.UI;
+using Zenject;
 
 namespace MokomoGames.UI
 {
@@ -10,44 +13,122 @@ namespace MokomoGames.UI
     {
         [SerializeField] private Button backButton;
         [SerializeField] private Button homeButton;
-
+        [SerializeField] private ToggleGroup toggleGroup;
+        [SerializeField] private Toggle soulToggle;
+        [SerializeField] private Toggle materialToggle;
         [SerializeField] private GameObject cellRow;
-        [SerializeField] private GameObject contentRoot;
+        [SerializeField] private RectTransform contentRoot;
+        [SerializeField] private VerticalLayoutGroup verticalRoot;
         [SerializeField] private UISoulCell soulCellPrefab;
+        private List<UISoulCell> _soulCells = new List<UISoulCell>();
+        private Tab CurrentTab => soulToggle.isOn == true ? Tab.Soul : Tab.Material;
+        private ColorBlock selectingColorBlock;
+        private ColorBlock unSelectingColorBlock;
         [Inject] private IMasterDataRepository _masterDataRepository;
         public event Action OnTappedHomeButton;
-
+        
+        public enum Tab
+        {
+            Soul,
+            Material,
+        }
+        
         private void Awake()
         {
+            foreach (var toggle in toggleGroup.GetComponentsInChildren<Toggle>())
+            {
+                toggle.onValueChanged.RemoveListener(OnChangedTab);
+                toggle.onValueChanged.AddListener(OnChangedTab);
+            }
+
             backButton.onClick.AddListener( () => gameObject.SetActive(false));
             homeButton.onClick.AddListener( () =>
             {
+                DestroyCells();
                 gameObject.SetActive(false);
                 OnTappedHomeButton?.Invoke();
             });
+            
+            selectingColorBlock = new ColorBlock();
+            selectingColorBlock.colorMultiplier = 1f;
+            selectingColorBlock.normalColor = Color.yellow;
+            selectingColorBlock.highlightedColor = Color.yellow;
+            selectingColorBlock.pressedColor = Color.yellow;
+            selectingColorBlock.selectedColor = Color.yellow;
+            
+            unSelectingColorBlock = new ColorBlock();
+            unSelectingColorBlock.colorMultiplier = 1f;
+            unSelectingColorBlock.normalColor = Color.gray;
+            unSelectingColorBlock.highlightedColor = Color.gray;
+            unSelectingColorBlock.pressedColor = Color.gray;
+            unSelectingColorBlock.selectedColor = Color.gray;
+        }
+
         public void Begin()
         {
+            soulToggle.isOn = true;
+        }
+
+        private List<UISoulCell> MakeCells(List<SoulTableRecord> records)
+        {
             const int cellNumPerRow = 6;
-            var records = _masterDataRepository.SoulTable.Records;
             var rowNum = Mathf.CeilToInt((float)records.Count / cellNumPerRow);
+            var soulCells = new List<UISoulCell>();
             for (int i = 0; i < rowNum; i++)
             {
                 var row = Instantiate(cellRow);
-                row.transform.SetParent(contentRoot.transform);
+                row.transform.SetParent(verticalRoot.transform);
                 row.transform.localScale = Vector3.one;
                 for (int j = 0; j < row.transform.childCount; j++)
                 {
                     var index = i * cellNumPerRow + j;
                     var soulCell = row.transform.GetChild(j).gameObject.GetComponent<UISoulCell>();
+                    soulCells.Add(soulCell);
                     var inRange = index < records.Count;
                     soulCell.Show(inRange);
                     if (!inRange)
                         continue;
                     var soulData = records[index];
-                    Debug.Log(soulCell.gameObject.name);
+                    soulCell.Begin(soulData);
                 }
             }
+
+            var height = rowNum * soulCells[0]?.Height ?? 0f;
+            height += verticalRoot.spacing;
+            contentRoot.sizeDelta = new Vector2(contentRoot.sizeDelta.x,height);
+            return soulCells;
         }
+
+        private void DestroyCells()
+        {
+            for (int i = 0; i < verticalRoot.transform.childCount; i++)
+            {
+                var obj = verticalRoot.transform.GetChild(i).gameObject;
+                Destroy(obj);
+            }
+        }
+
+        private void OnChangedTab(bool isOn)
+        {
+            soulToggle.colors = soulToggle.isOn == isOn ? selectingColorBlock : unSelectingColorBlock;
+            materialToggle.colors = materialToggle.isOn == isOn ? selectingColorBlock : unSelectingColorBlock;
+
+            if (CurrentTab == Tab.Material)
+            {
+                DestroyCells();
+                var materials = _masterDataRepository.SoulTable.Records
+                    .Where(x => x.IsMaterial())
+                    .ToList();
+                MakeCells(materials);
+            }
+            else if(CurrentTab == Tab.Soul)
+            {
+                DestroyCells();
+                var souls = _masterDataRepository.SoulTable.Records
+                    .Where(x => !x.IsMaterial())
+                    .ToList();
+                MakeCells(souls);
+            }
         }
     }
 }
