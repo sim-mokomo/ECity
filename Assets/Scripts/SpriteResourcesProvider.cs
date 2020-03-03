055ba8a8547d489305d42771a0ec581010fee833
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-03-04 02:39:01 +0900

    impl: master data に対応したアイコンを表示するように

---
+++ Assets/Scripts/SpriteResourcesProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MokomoGames.Protobuf;
+using UnityEngine;
+
+public class SpriteResourcesProvider
+{
+    private static readonly Dictionary<Attribute, string> attributeSpriteTable = new Dictionary<Attribute, string>()
+    {
+        {Attribute.Fire,"FireIcon"},
+        {Attribute.Water,"WaterIcon"},
+        {Attribute.Wood,"WoodIcon"},
+        {Attribute.Light,"LightIcon"},
+        {Attribute.Shadow,"ShadowIcon"},
+    };
+    public static Sprite GetAttributeIcon(Attribute attribute)
+    {
+        attributeSpriteTable.TryGetValue(attribute, out var path);
+        var sprite = Resources.Load<Sprite>(path);
+        return sprite;
+    }
+
+    public static Sprite GetCharacterIcon(string spriteName)
+    {
+        var sprite = Resources.Load<Sprite>(spriteName);
+        return sprite;
+    }
+}
--- Assets/Scripts/UI/UISoulCell.cs
+++ Assets/Scripts/UI/UISoulCell.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using MokomoGames.Protobuf;
 using UI;
 using UnityEngine;
 using UnityEngine.UI;
+using Attribute = MokomoGames.Protobuf.Attribute;
 
 namespace MokomoGames.UI
 {
@@ -12,9 +14,19 @@ namespace MokomoGames.UI
         public float Width => _rectTransform.rect.width;
         private RectTransform _rectTransform;
         [SerializeField] private Image attributeIcon;
+        [SerializeField] private Image attributeBackgroundImage;
         [SerializeField] private Image characterIcon;
         [SerializeField] private UIStars stars;
         
+        //TODO: sprite に置き換えられる予定
+        private static readonly Dictionary<Attribute,Color> attributeColorTable = new Dictionary<Attribute, Color>()
+        {
+            {Attribute.Fire,new Color(1f, 0.33f, 0.33f)},
+            {Attribute.Water,new Color(0.38f, 0.39f, 1f)},
+            {Attribute.Wood,new Color(0.41f, 1f, 0.42f)},
+            {Attribute.Light,new Color(1f, 0.76f, 0.25f)},
+            {Attribute.Shadow,new Color(0.89f, 0.37f, 1f)},
+        };
         
         private void Awake()
         {
@@ -23,8 +35,10 @@ namespace MokomoGames.UI
 
         public void Begin(SoulTableRecord record)
         {
-            //TODO: 属性アイコン
-            //TODO: キャラ画像
+            attributeIcon.sprite = SpriteResourcesProvider.GetAttributeIcon(record.Attribute);
+            characterIcon.sprite = SpriteResourcesProvider.GetCharacterIcon(record.CharacterIconName);
+            attributeColorTable.TryGetValue(record.Attribute,out var backgroundColor);
+            attributeBackgroundImage.color = backgroundColor; 
             stars.Show(record.Rarity);
         }
         
