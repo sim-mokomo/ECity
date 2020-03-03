50f2641ac362a8c46007ea6d9162becbbf6463a2
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-03-04 01:52:14 +0900

    impl: レアリティ表示用UI作成

---
+++ Assets/Scripts/UI/UIStars.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UIStars : MonoBehaviour
+    {
+        [SerializeField] private List<Image> stars;
+        [SerializeField] private RectTransform horizontalLayoutRectTransform;
+        [SerializeField] private HorizontalLayoutGroup horizontalLayoutGroup;
+
+        public void Show(uint starNum)
+        {
+            for (int i = 0; i < stars.Count; i++)
+            {
+                var star = stars[i];
+                star.gameObject.SetActive(i < starNum);
+            }
+
+            var sample = stars.FirstOrDefault();
+            horizontalLayoutRectTransform.sizeDelta = new Vector2(sample.rectTransform.sizeDelta.x * starNum + horizontalLayoutGroup.spacing * starNum,horizontalLayoutRectTransform.sizeDelta.y);
+        }
+    }
+}
