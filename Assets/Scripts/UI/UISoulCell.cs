d104f5681e4330532dc2cd94c89cd07642bb4b15
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-03-04 01:52:34 +0900

    impl: レアリティ表示ができるように

--- Assets/Scripts/UI/UISoulCell.cs
+++ Assets/Scripts/UI/UISoulCell.cs
@@ -1,9 +1,33 @@
-﻿using UnityEngine;
+﻿using System;
+using MokomoGames.Protobuf;
+using UI;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace MokomoGames.UI
 {
     public class UISoulCell : MonoBehaviour
     {
+        public float Height => _rectTransform.rect.height;
+        public float Width => _rectTransform.rect.width;
+        private RectTransform _rectTransform;
+        [SerializeField] private Image attributeIcon;
+        [SerializeField] private Image characterIcon;
+        [SerializeField] private UIStars stars;
+        
+        
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+        }
+
+        public void Begin(SoulTableRecord record)
+        {
+            //TODO: 属性アイコン
+            //TODO: キャラ画像
+            stars.Show(record.Rarity);
+        }
+        
         public void Show(bool show)
         {
             for (int i = 0; i < transform.childCount; i++)
