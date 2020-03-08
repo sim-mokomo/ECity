b61cce0f5ae683084ffc26aaa7827ce393a6c711
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-03-08 22:59:42 +0900

    impl: enum に拡張メソッドを移動

---
+++ Assets/Scripts/SoulTypeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using MokomoGames.Protobuf;
+
+namespace MokomoGames
+{
+    public static class SoulTypeExtensions
+    {
+        public static bool IsMaterial(this SoulType self)
+        {
+            return self == SoulType.Evolution ||
+                   self == SoulType.Sale ||
+                   self == SoulType.ReinforcedSynthesis;
+        }
+    }
+}
