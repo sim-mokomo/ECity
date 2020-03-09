604990acfbdcee702a825cfbf0007e021f9cb827
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-03-09 23:34:22 +0900

    impl: 属性Enumを文字に変換するように

---
+++ Assets/Scripts/AttributeEnumExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Attribute = MokomoGames.Protobuf.Attribute;
+
+namespace MokomoGames
+{
+    public static class AttributeEnumExtensions
+    {
+        public static string GetName(this Attribute self)
+        {
+            switch (self)
+            {
+                case Attribute.Fire:
+                    return "火属性";
+                case Attribute.Water:
+                    return "水属性";
+                case Attribute.Wood:
+                    return "木属性";
+                case Attribute.Light:
+                    return "光属性";
+                case Attribute.Shadow:
+                    return "闇属性";
+                case Attribute.Unknown:
+                default:
+                    return "???";
+            }
+        }
+    }
+}
