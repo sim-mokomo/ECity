fc8acff8159f824fa6a6e4536ba61d2793f6f5e0
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-03-09 20:20:48 +0900

    impl: 魂データを繋ぎ合わせるクラスの作成

---
+++ Assets/Scripts/UserSoulDataContainer.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using MokomoGames.Protobuf;
+
+namespace MokomoGames
+{
+    public class UserSoulDataContainer
+    {
+        private UserSoulData _userSoulData;
+        private SoulTableRecord _baseConfig;
+        private SoulLevelTableRecord _levelTableRecord;
+        public SoulTableRecord BaseConfig => _baseConfig;
+
+        public UserSoulDataContainer(UserSoulData userSoulData,IMasterDataRepository masterDataRepository,IPlayerSaveDataRepository playerSaveDataRepository)
+        {
+            _userSoulData = userSoulData;
+            _baseConfig = masterDataRepository.SoulTable.Records.FirstOrDefault(x => x.No == _userSoulData.SoulNo);
+
+            var soulLevelRecords = masterDataRepository.SoulLevelTable.Records;
+            var totalExp = 0u;
+            foreach (var record in soulLevelRecords)
+            {
+                totalExp += record.NeedNextLevelExp;
+                if (totalExp <= _userSoulData.TotalLevelExp)
+                {
+                    _levelTableRecord = record;
+                }
+            }
+
+            _levelTableRecord = soulLevelRecords.FirstOrDefault(x =>
+                x.No == _userSoulData.SoulNo && x.Level == _levelTableRecord.Level);
+        }
+    }
+}
