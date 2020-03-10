9baf0309a83757f4639be5ef61ab354e1eba392e
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-03-11 00:11:59 +0900

    impl: 魂の詳細画面実装

---
+++ Assets/Scripts/UI/UISoulDetailPage.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MokomoGames.UI
+{
+    public class UISoulDetailPage : MonoBehaviour
+    {
+        // 属性アイコン
+        [SerializeField] private TextMeshProUGUI elementNameText;
+        [SerializeField] private Image elementIcon;
+        // パラメータ群
+        [SerializeField] private TextMeshProUGUI hpText;
+        [SerializeField] private TextMeshProUGUI recoveryPowerText;
+        [SerializeField] private TextMeshProUGUI attackText;
+        [SerializeField] private TextMeshProUGUI costText;
+        // レベル
+        [SerializeField] private TextMeshProUGUI levelText;
+        [SerializeField] private UIGaugeWithUpperLabel expGauge;
+        // ノーマルスキル
+        [SerializeField] private TextMeshProUGUI normalSkillNameText;
+        [SerializeField] private UIGaugeWithUpperLabel normalSkillGauge;
+        [SerializeField] private TextMeshProUGUI normalSkillLevelText;
+        [SerializeField] private TextMeshProUGUI normalSkillContentText;
+        [SerializeField] private TextMeshProUGUI normalSkillChargeTurnText;
+        // リーダースキル
+        [SerializeField] private TextMeshProUGUI readerSkillNameText;
+        [SerializeField] private TextMeshProUGUI readerSkillContentText;
+        // 名前
+        [SerializeField] private TextMeshProUGUI subNameText;
+        [SerializeField] private TextMeshProUGUI mainNameText;
+        [SerializeField] private UIStars rarityUI;
+        // タイプ
+        [SerializeField] private TextMeshProUGUI noText;
+        [SerializeField] private TextMeshProUGUI typeText;
+        [SerializeField] private Button favoriteButton;
+
+        [SerializeField] private Button backButton;
+        public event Action OnTappedBackButton;
+
+        private void Awake()
+        {
+            favoriteButton.onClick.AddListener(() =>
+            {
+                //TODO: identity id を元にお気に入り登録をする
+            });
+            
+            backButton.onClick.AddListener(() =>
+            {
+                OnTappedBackButton?.Invoke();
+            });
+        }
+
+        public void Begin(UserSoulDataContainer soulDataContainer)
+        {
+            subNameText.text = soulDataContainer.BaseConfig.AnotherName;
+            mainNameText.text = soulDataContainer.BaseConfig.Name;
+            rarityUI.Show(soulDataContainer.BaseConfig.Rarity);
+
+            levelText.text = $"{soulDataContainer.LevelTableRecord.Level}/{999}";
+            hpText.text = $"{soulDataContainer.LevelTableRecord.Hp}";
+            attackText.text = $"{soulDataContainer.LevelTableRecord.Power}";
+            recoveryPowerText.text = $"{soulDataContainer.LevelTableRecord.RecoveryPower}";
+            costText.text = $"{soulDataContainer.BaseConfig.Cost}";
+            
+            elementIcon.sprite = SpriteResourcesProvider.GetAttributeIcon(soulDataContainer.BaseConfig.Attribute);
+            elementNameText.text = soulDataContainer.BaseConfig.Attribute.GetName();
+            expGauge.SetRemainingValue($"{soulDataContainer.GetRemainingLevelExp()}");
+            expGauge.SetRemainingSliderValue(
+                soulDataContainer.UserSoulData.TotalLevelExp, 
+                soulDataContainer.GetTotalNeedLevelExp());
+            
+            normalSkillNameText.text = soulDataContainer.NormalSkillTableRecord.Name;
+            normalSkillContentText.text = soulDataContainer.NormalSkillTableRecord.Description;
+            normalSkillLevelText.text = soulDataContainer.NormalSkillLevelTableRecord.Level.ToString();
+            normalSkillGauge.SetRemainingValue($"{soulDataContainer.GetRemainingNormalSkillExp()}");
+            normalSkillGauge.SetRemainingSliderValue(
+                soulDataContainer.UserSoulData.TotalSkillExp,
+                soulDataContainer.GetTotalNeedNormalSkillLevelExp());
+            normalSkillChargeTurnText.text = $"ターン:{soulDataContainer.NormalSkillTableRecord.BaseTurn}";
+
+            readerSkillNameText.text = soulDataContainer.ReaderSkillTableRecord.Name;
+            readerSkillContentText.text = soulDataContainer.ReaderSkillTableRecord.Description;
+
+            noText.text = $"{soulDataContainer.BaseConfig.No}";
+            typeText.text = $"{soulDataContainer.BaseConfig.SoulType.GetName()}";
+        }
+
+        public void Show(bool show)
+        {
+            gameObject.SetActive(show);
+        }
+    }
+}
--- Assets/Scripts/UserSoulDataContainer.cs
+++ Assets/Scripts/UserSoulDataContainer.cs
@@ -8,26 +8,96 @@ namespace MokomoGames
         private UserSoulData _userSoulData;
         private SoulTableRecord _baseConfig;
         private SoulLevelTableRecord _levelTableRecord;
+        private IMasterDataRepository _masterDataRepository;
+        private NormalSkillTableRecord _normalSkillTableRecord;
+        private NormalSkillLevelTableRecord _normalSkillLevelTableRecord;
+        private ReaderSkillTableRecord _readerSkillTableRecord;
         public SoulTableRecord BaseConfig => _baseConfig;
+        public UserSoulData UserSoulData => _userSoulData;
+        public SoulLevelTableRecord LevelTableRecord => _levelTableRecord;
+        public NormalSkillTableRecord NormalSkillTableRecord => _normalSkillTableRecord;
+        public NormalSkillLevelTableRecord NormalSkillLevelTableRecord => _normalSkillLevelTableRecord;
+        public ReaderSkillTableRecord ReaderSkillTableRecord => _readerSkillTableRecord;
 
         public UserSoulDataContainer(UserSoulData userSoulData,IMasterDataRepository masterDataRepository,IPlayerSaveDataRepository playerSaveDataRepository)
         {
             _userSoulData = userSoulData;
             _baseConfig = masterDataRepository.SoulTable.Records.FirstOrDefault(x => x.No == _userSoulData.SoulNo);
+            _masterDataRepository = masterDataRepository;
+            _normalSkillTableRecord =
+                masterDataRepository.NormalSkillTable.Records.FirstOrDefault(x => x.No == BaseConfig.NormalSkillId);
+            _readerSkillTableRecord =
+                masterDataRepository.ReaderSkillTable.Records.FirstOrDefault(x => x.No == BaseConfig.ReaderSkillId);
+            _levelTableRecord = GetLevelRecord(masterDataRepository.SoulLevelTable,_userSoulData.TotalLevelExp);
+            _normalSkillLevelTableRecord = GetNormalSkillLevelRecord(masterDataRepository.NormalSkillLevelTable,
+                _userSoulData.TotalSkillExp);
+        }
 
-            var soulLevelRecords = masterDataRepository.SoulLevelTable.Records;
+        private SoulLevelTableRecord GetLevelRecord(SoulLevelTable soulLevelTable,uint totalLevelExp)
+        {
             var totalExp = 0u;
-            foreach (var record in soulLevelRecords)
+            var res = new SoulLevelTableRecord()
             {
+                Hp = 1,
+                Level = 1,
+                No = 1,
+                Power = 1,
+                RecoveryPower = 1,
+                NeedNextLevelExp = 200
+            };
+            foreach (var record in soulLevelTable.Records
+                .Where(x => x.No == _userSoulData.SoulNo))
+            {
+                res = record;
                 totalExp += record.NeedNextLevelExp;
-                if (totalExp <= _userSoulData.TotalLevelExp)
+                if (totalExp >= totalLevelExp)
                 {
-                    _levelTableRecord = record;
+                    break;
                 }
             }
+            return res;
+        }
+        
+        private NormalSkillLevelTableRecord GetNormalSkillLevelRecord(NormalSkillLevelTable normalSkillLevelTable,uint totalSkillExp)
+        {
+            var totalExp = 0u;
+            var res = new NormalSkillLevelTableRecord();
+            foreach (var record in normalSkillLevelTable.Records)
+            {
+                res = record;
+                totalExp += record.NeedNextLevelExp;
+                if (totalExp >= totalSkillExp)
+                {
+                    break;
+                }
+            }
+            return res;
+        }
 
-            _levelTableRecord = soulLevelRecords.FirstOrDefault(x =>
-                x.No == _userSoulData.SoulNo && x.Level == _levelTableRecord.Level);
+        public uint GetRemainingLevelExp()
+        {
+            return GetTotalNeedLevelExp() - _userSoulData.TotalLevelExp;
+        }
+        
+        public uint GetTotalNeedLevelExp()
+        {
+            var records = _masterDataRepository.SoulLevelTable.Records
+                .Where(x => x.No == _levelTableRecord.No)
+                .Where(x => x.Level <= _levelTableRecord.Level);
+            var totalExp= records.Any() ? records.Sum(x => x.NeedNextLevelExp) : 200;
+            return 200;
+        }
+
+        public uint GetRemainingNormalSkillExp()
+        {
+            return GetTotalNeedNormalSkillLevelExp() - _userSoulData.TotalSkillExp;
+        }
+
+        public uint GetTotalNeedNormalSkillLevelExp()
+        {
+            return (uint)_masterDataRepository.NormalSkillLevelTable.Records
+                .Where(x => x.Level <= _normalSkillLevelTableRecord.Level)
+                .Sum(x => x.NeedNextLevelExp);
         }
     }
 }
