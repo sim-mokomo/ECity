9cf8396de1db38e7df25fc4ed14197a982399cd2
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-03-11 00:09:29 +0900

    impl: 魂→文字列変換

---
+++ Assets/Scripts/SoulTypeEnumExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MokomoGames.Protobuf;
+
+namespace MokomoGames
+{
+    public static class SoulTypeEnumExtensions
+    {
+        public static string GetName(this SoulType self)
+        {
+            switch (self)
+            {
+                case SoulType.Art:
+                    return "アート系";
+                case SoulType.Underground:
+                    return "アングラ系";
+                case SoulType.Cure:
+                    return "癒し系";
+                case SoulType.Toughness:
+                    return "体力系";
+                case SoulType.Transcendence:
+                    return "超越系";
+                case SoulType.Balance:
+                    return "バランス系";
+                case SoulType.ReinforcedSynthesis:
+                    return "強化合成";
+                case SoulType.Evolution:
+                    return "進化";
+                case SoulType.Sale:
+                    return "売却";
+                case SoulType.Unknown:
+                default:
+                    return "???";
+            }
+        }
+    }
+}
