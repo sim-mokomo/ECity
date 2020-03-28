547ef3837e4b6b8cdb50971157ce306abc0913d6
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-03-28 13:27:53 +0900

    impl: 子を一括で取得する拡張を実装

---
+++ Assets/Scripts/Extensions/TransformExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UniRx.Async;
+using UnityEngine;
+
+namespace MokomoGames
+{
+    public static class TransformExtensions
+    {
+        public static IEnumerable<GameObject> Childs(this Transform self)
+        {
+            return Enumerable
+                .Range(0, self.childCount)
+                .Select(x => self.GetChild(x).gameObject);
+        }
+    }
+}
