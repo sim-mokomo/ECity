f30fb4be2982e5bba505a2a7e03d76c631d56267
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-03-28 19:48:30 +0900

    impl: 売却画面を作成

--- Assets/Scripts/UI/UISoulSalePage.cs
+++ Assets/Scripts/UI/UISoulSalePage.cs
@@ -1,16 +1,53 @@
 ﻿using System;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MokomoGames.UI
 {
     public class UISoulSalePage : MonoBehaviour, IPage
     {
-        public enum Tab
+        private UserSoulList userSoulList;
+        [SerializeField] private Button backButton;
+        [SerializeField] private Button homeButton;
+        [SerializeField] private UITab tab;
+        [SerializeField] private UICellScroll _cellScroll;
+        [SerializeField] private UIHasNumSolidLabel soulHasNumSolidLabel;
+        public event Action OnTappedHomeButton;
+
+        public void SetData(UserSoulList userSoulList)
         {
-            Soul,
-            Material
+            this.userSoulList = userSoulList;
         }
 
-        public event Action OnTappedHomeButton;
+        private void Awake()
+        {
+            tab.OnChangedTab += tabElement =>
+            {
+                _cellScroll.DestroyCells();
+                var showSouls = tabElement.TabType == UITab.TabType.Battle
+                    ? userSoulList.GetBattleSouls()
+                    : userSoulList.GetMaterialSouls();
+                
+                soulHasNumSolidLabel.UpdateHasNum((uint)showSouls.Count(),9999);
+                _cellScroll.MakeCells(showSouls.ToList(), soul =>
+                {
+                    //TODO: 売却対象として選択できるように
+                });
+            };
+            
+            backButton.onClick.AddListener( () => gameObject.SetActive(false));
+            homeButton.onClick.AddListener(() =>
+            {
+                _cellScroll.DestroyCells();
+                gameObject.SetActive(false);
+                OnTappedHomeButton?.Invoke();
+            });
+        }
+
+        public void Begin()
+        {
+            tab.Begin(UITab.TabType.Battle);
+        }
     }
 }
