dfe13766b3fc91a38fc4f82f554bc4c905370097
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-03-28 13:41:22 +0900

    impl: split file about tab element

---
+++ Assets/Scripts/UI/UITabElement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MokomoGames.UI
+{
+    [Serializable]
+    public class UITabElement
+    {
+        [SerializeField] private UITab.TabType _tabType;
+        [SerializeField] private Toggle _toggle;
+
+        public Toggle Toggle => _toggle;
+        public UITab.TabType TabType => _tabType;
+    }
+}
