6aa382a8f086e88428e212b44e74fd1a199a2d3c
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2020-03-28 13:27:26 +0900

    impl: スクロールビュー処理を分離

---
+++ Assets/Scripts/UI/UICellScroll.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MokomoGames.UI
+{
+    public class UICellScroll : MonoBehaviour
+    {
+        [SerializeField] private GameObject cellRow;
+        [SerializeField] private VerticalLayoutGroup verticalRoot;
+        [SerializeField] private RectTransform contentRoot;
+
+        public List<UISoulCell> MakeCells(List<Soul> records, Action<Soul> OnTappedSoulCellIcon)
+        {
+            const int cellNumPerRow = 6;
+            var rowNum = Mathf.CeilToInt((float) records.Count / cellNumPerRow);
+            var soulCells = new List<UISoulCell>();
+            
+            for (var i = 0; i < rowNum; i++)
+            {
+                var row = Instantiate(cellRow);
+                row.transform.SetParent(verticalRoot.transform);
+                row.transform.localScale = Vector3.one;
+                
+                for (var j = 0; j < row.transform.childCount; j++)
+                {
+                    var index = i * cellNumPerRow + j;
+                    var soulCell = row.transform.GetChild(j).gameObject.GetComponent<UISoulCell>();
+                    soulCells.Add(soulCell);
+                    
+                    var inRange = index < records.Count;
+                    soulCell.Show(inRange);
+                    if (!inRange)
+                        continue;
+                    
+                    var soulData = records[index];
+                    soulCell.Begin(soulData);
+                    soulCell.OnTappedIcon += OnTappedSoulCellIcon;
+                }
+            }
+
+            var height = rowNum * soulCells[0]?.Height ?? 0f;
+            height += verticalRoot.spacing;
+            height += verticalRoot.padding.top;
+            contentRoot.sizeDelta = new Vector2(contentRoot.sizeDelta.x, height);
+            
+            return soulCells;
+        }
+
+        public void DestroyCells()
+        {
+            foreach (var child in verticalRoot.transform.Childs())
+            {
+                Destroy(child.gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/UI/UISoulListPage.cs
+++ Assets/Scripts/UI/UISoulListPage.cs
@@ -16,23 +16,17 @@ namespace MokomoGames.UI
             Material
         }
 
-        [Inject] private IMasterDataRepository _masterDataRepository;
-        [Inject] private IPlayerSaveDataRepository _playerSaveDataRepository;
-        private List<UISoulCell> _soulCells = new List<UISoulCell>();
         [SerializeField] private Button backButton;
-        [SerializeField] private GameObject cellRow;
-        [SerializeField] private RectTransform contentRoot;
         [SerializeField] private TextMeshProUGUI hasSoulNum;
         [SerializeField] private Button homeButton;
         [SerializeField] private Toggle materialToggle;
         private ColorBlock selectingColorBlock;
-        [SerializeField] private UISoulCell soulCellPrefab;
         [SerializeField] private UISoulDetailPage soulDetailPage;
         [SerializeField] private Toggle soulToggle;
         [SerializeField] private ToggleGroup toggleGroup;
         private ColorBlock unSelectingColorBlock;
         private UserSoulList _userSoulList;
-        [SerializeField] private VerticalLayoutGroup verticalRoot;
+        [SerializeField] private UICellScroll soulCellScroll;
         private Tab CurrentTab => soulToggle.isOn ? Tab.Soul : Tab.Material;
         public event Action OnTappedHomeButton;
 
@@ -47,7 +41,7 @@ namespace MokomoGames.UI
             backButton.onClick.AddListener(() => gameObject.SetActive(false));
             homeButton.onClick.AddListener(() =>
             {
-                DestroyCells();
+                soulCellScroll.DestroyCells();
                 gameObject.SetActive(false);
                 OnTappedHomeButton?.Invoke();
             });
@@ -86,51 +80,6 @@ namespace MokomoGames.UI
             hasSoulNum.text = $"{currentCount}/{hasSoulCapacity}";
         }
 
-        private List<UISoulCell> MakeCells(List<Soul> records)
-        {
-            const int cellNumPerRow = 6;
-            var rowNum = Mathf.CeilToInt((float) records.Count / cellNumPerRow);
-            var soulCells = new List<UISoulCell>();
-            for (var i = 0; i < rowNum; i++)
-            {
-                var row = Instantiate(cellRow);
-                row.transform.SetParent(verticalRoot.transform);
-                row.transform.localScale = Vector3.one;
-                for (var j = 0; j < row.transform.childCount; j++)
-                {
-                    var index = i * cellNumPerRow + j;
-                    var soulCell = row.transform.GetChild(j).gameObject.GetComponent<UISoulCell>();
-                    soulCells.Add(soulCell);
-                    var inRange = index < records.Count;
-                    soulCell.Show(inRange);
-                    if (!inRange)
-                        continue;
-                    var soulData = records[index];
-                    soulCell.Begin(soulData);
-                    soulCell.OnTappedIcon += OnTappedSoulCellIcon;
-                }
-            }
-
-            var height = rowNum * soulCells[0]?.Height ?? 0f;
-            height += verticalRoot.spacing;
-            contentRoot.sizeDelta = new Vector2(contentRoot.sizeDelta.x, height);
-            return soulCells;
-        }
-
-        private void OnTappedSoulCellIcon(Soul soul)
-        {
-            soulDetailPage.Show(true);
-            soulDetailPage.Begin(soul);
-        }
-
-        private void DestroyCells()
-        {
-            for (var i = 0; i < verticalRoot.transform.childCount; i++)
-            {
-                var obj = verticalRoot.transform.GetChild(i).gameObject;
-                Destroy(obj);
-            }
-        }
 
         private void OnChangedTab(bool isOn)
         {
@@ -138,22 +87,22 @@ namespace MokomoGames.UI
 
             if (CurrentTab == Tab.Material)
             {
-                DestroyCells();
+                soulCellScroll.DestroyCells();
 
                 var materials = _userSoulList.ExistMaterialSouls
                     ? _userSoulList.GetMaterialSouls()
                     : UserSoulList.Empty;
                 UpdateHasSoulNum(materials.Count());
-                MakeCells(materials.ToList());
+                soulCellScroll.MakeCells(materials.ToList(), TappedSoulCellIcon);
             }
             else if (CurrentTab == Tab.Soul)
             {
-                DestroyCells();
+                soulCellScroll.DestroyCells();
                 var souls = _userSoulList.ExistBattleSouls
                     ? _userSoulList.GetBattleSouls()
                     : UserSoulList.Empty;
                 UpdateHasSoulNum(souls.Count());
-                MakeCells(souls.ToList());
+                soulCellScroll.MakeCells(souls.ToList(),TappedSoulCellIcon);
             }
         }
 
